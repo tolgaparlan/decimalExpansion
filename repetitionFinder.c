283db16ce9cc3e5d4279e4dd8b32c817b884c113
Tolga <tolgaparlan@gmail.com>
2016-10-11 20:17:02 +0200

    Added the isPrime function from the midterm

--- repetitionFinder.c
+++ repetitionFinder.c
@@ -5,6 +5,22 @@
 
 #include <stdio.h>
 
+int isPrime(int num){ //checks if a number is prime or not
+	if(num==2 || num==3 || num==5){ //basic cases
+		return 1;
+	}
+	if (num%2==0 || num%3==0 || num==1 ||num%5==0){ //basic cases
+		return 0;
+	}
+	for(int i=6;i*i<num;i+=6){ 
+		if(num%(i+1)==0 || num%(i-1)==0){ //every prime number is in the form of 6k+-1
+			return 0; //not prime
+		}		
+	}
+	return 1; //prime
+}
+
+
 int main(int argc, char **argv)
 {
 	
